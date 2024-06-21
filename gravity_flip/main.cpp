97c63f19834a3f33e3eb9cbd33c67dc62f0740fc
Khaled Empeshy <empeshy20036969@gmail.com>
2024-06-21 05:40:35 +0300

    using_sort_std

--- gravity_flip/main.cpp
+++ gravity_flip/main.cpp
@@ -1,15 +1,14 @@
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
 
 int main() {
 	// n => number of cols
 	// cols => number of cubes in col
-	int *cols, tmp;
+	int *cols;
 	unsigned long n;
-	bool swapped = false;
 
 	std::cin >> n;
-	
 	// create cubes array
 	cols = (int *) malloc(n * sizeof(int));
 	for (int i = 0; i < (int)n; i++) {
@@ -18,23 +17,7 @@ int main() {
 
 	// gravity flip
 	// sort
-	for (int i = 0; i < (int)n && !swapped; i++) {
-		for (int j = 0; j < (int)n-i-1; j++) {
-			if (cols[j+1] < cols[j]) {
-				swapped = true;
-				tmp = cols[j];
-				cols[j] = cols[j+1];
-				cols[j+1] = tmp;
-			}
-			// print answer
-			for (int i = 0; i < (int)n-1; i++) {
-				std::cout << cols[i] << " ";
-			}
-			std::cout << cols[n-1] << std::endl;
-
-		}
-		swapped = false;
-}
+	std::sort(cols, cols+n);
 
 	// print answer
 	for (int i = 0; i < (int)n-1; i++) {
