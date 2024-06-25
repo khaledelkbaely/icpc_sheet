f37bb887f08f53dc237562f113f8d45d74f4d000
Khaled Empeshy <empeshy20036969@gmail.com>
2024-06-25 14:53:20 +0300

    word

---
+++ word/main.cpp
@@ -0,0 +1,28 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string word;
+	int upper, lower;
+	upper = lower = 0;
+
+	std::cin >> word;
+	
+	char *p = &word[0];
+	while (*p) {
+		if (islower(*p))
+			lower++;
+		else
+			upper++;
+		p++;
+	}
+
+	if (upper > lower)
+		for (char &c : word)
+			c = (char)std::toupper(c);
+	else
+		for (char &c : word)
+			c = (char)std::tolower(c);
+	std::cout << word;
+}
