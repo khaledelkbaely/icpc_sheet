c094633daf8d8b698812bfc2757f506fb914e77d
Khaled Empeshy <empeshy20036969@gmail.com>
2024-06-29 13:33:20 +0300

    night_at_the_museum

---
+++ night_at_the_museum/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	char curr{'a'};
+	std::string name;
+	int rotations{};
+
+	std::cin >> name;
+
+	for (auto c : name) {
+		if (abs(c - curr) > 13)
+			rotations += 26 - abs(c - curr);
+		else if (c - curr <= 13)
+			rotations += abs(c - curr);
+		curr = c;
+	}
+	std::cout << rotations;
+}
