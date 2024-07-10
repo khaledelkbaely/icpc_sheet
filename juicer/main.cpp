1d3dbe3dd65dd95670b80bd7db137521ecb94e77
Khaled Empeshy <empeshy20036969@gmail.com>
2024-07-11 00:39:04 +0300

    juicer

---
+++ juicer/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+	int number_of_oranges{}, max_orange_size{}, waste_limit{};
+	int total{}, times_to_empty_waste{};
+
+	std::cin >> number_of_oranges
+		 >> max_orange_size
+		 >> waste_limit;
+	for (int i = 0; i < number_of_oranges; ++i) {
+		int orange{};
+		std::cin >> orange;
+
+		if (orange <= max_orange_size)
+			total += orange;
+
+		if (total > waste_limit) {
+			times_to_empty_waste++;
+			total -= waste_limit;
+		}
+	}
+	std::cout << times_to_empty_waste;
+}
