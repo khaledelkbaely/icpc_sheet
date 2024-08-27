d6e654373575226b6f012372817f4572f2598245
Khaled Empeshy <empeshy20036969@gmail.com>
2024-08-28 00:36:49 +0300

    UVA10106

---
+++ UVA/10106/main2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main(int argc, char *argv[]) {
+    std::string n1, n2;
+
+    while (std::cin >> n1 >> n2) {
+        std::vector<int> nums_reversed(300);
+
+        for (int i = 0; i < n1.size(); i++) {
+            for (int j = 0; j < n2.size(); j++) {
+                nums_reversed[i+j] += (n1[i] - '0') * (n2[j] - '0');
+            }
+        }
+
+        auto it = nums_reversed.end()-1;
+        while (it != nums_reversed.begin()-1 && *it == 0) it--;
+        while (it != nums_reversed.begin()-1) std::cout << *it--;
+
+        std::cout << std::endl;
+
+    }
+    return 0;
+}
