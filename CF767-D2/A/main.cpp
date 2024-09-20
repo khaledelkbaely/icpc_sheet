11db72fd56fb5cd1fb9be6816da213a6698edd4b
Khaled Empeshy <empeshy20036969@gmail.com>
2024-09-20 21:28:58 +0300

    CF767-D2-A

---
+++ CF767-D2/A/main.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main (int argc, char *argv[]) {
+    int n{};
+    std::cin >> n;
+
+    std::vector<int> skipped(n, 0);
+
+    int max = n;
+    for (int i = 0; i < n; i++) {
+        int curr;
+        std::cin >> curr;
+
+        if (curr == max) {
+            std::cout << curr << ' ';
+            max--;
+            for (int j = n-1; j >= 0; j--) {
+                if (skipped[j]){
+                    std::cout << skipped[j] << ' ';
+                    skipped[j] = 0;
+                    max--;
+                }
+            }
+            std::cout << "\b\n";
+        } else {
+            skipped[curr-1] = curr;
+            std::cout << '\n';
+        }
+    }
+
+
+    return 0;
+}
