74516a5d50206c7e4e920e044660a8554ce845b9
Khaled Empeshy <empeshy20036969@gmail.com>
2024-09-21 21:36:09 +0300

    CF768-D2-A

--- CF768-D2/A/main.cpp
+++ CF768-D2/A/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <limits>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     int n{};
 
     std::cin >> n;
 
-    int min{100000000}, max{-1};
+    int min{std::numeric_limits<int>::max()},
+    max{std::numeric_limits<int>::min()};
     int count{}, max_cnt{}, min_cnt{};
     for (; n > 0; n--) {
         int a{};
@@ -14,23 +16,21 @@ int main (int argc, char *argv[]) {
         if (a > max) {
             max = a;
             max_cnt = 1;
-        } else if (a == max){
+        } else if (a == max) {
             max_cnt++;
         }
-        if (a < min){
+        if (a < min) {
             min = a;
             min_cnt = 1;
-        } else if (a == min){
+        } else if (a == min) {
             min_cnt++;
         }
 
         count++;
     }
-    if ( max_cnt + min_cnt >= count)
+    if (max_cnt + min_cnt >= count)
         std::cout << 0 << '\n';
     else
         std::cout << count - max_cnt - min_cnt << '\n';
-
-
     return 0;
 }
