51916bb1fa50c3f8c1ac743c963132ce6fdeddc7
Khaled Empeshy <empeshy20036969@gmail.com>
2024-09-21 21:18:51 +0300

    CF768-D2-A

--- CF768-D2/A/main.cpp
+++ CF768-D2/A/main.cpp
@@ -5,16 +5,31 @@ int main (int argc, char *argv[]) {
 
     std::cin >> n;
 
-    int count{};
+    int min{100000000}, max{-1};
+    int count{}, max_cnt{}, min_cnt{};
     for (; n > 0; n--) {
         int a{};
         std::cin >> a;
+
+        if (a > max) {
+            max = a;
+            max_cnt = 1;
+        } else if (a == max){
+            max_cnt++;
+        }
+        if (a < min){
+            min = a;
+            min_cnt = 1;
+        } else if (a == min){
+            min_cnt++;
+        }
+
         count++;
     }
-    if (count > 2)
-        std::cout << count-2 << '\n';
-    else
+    if ( max_cnt + min_cnt >= count)
         std::cout << 0 << '\n';
+    else
+        std::cout << count - max_cnt - min_cnt << '\n';
 
 
     return 0;
